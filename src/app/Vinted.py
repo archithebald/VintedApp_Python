bd500d67db72b5a9643cdcec27b393fa458ab2ae
BuildTools <unconfigured@null.spigotmc.org>
2024-06-02 11:57:17 +0200

    Added manual tests for main app ✅

--- src/app/Vinted.py
+++ src/app/Vinted.py
@@ -1,17 +1,21 @@
-from utils import cookies
-from api.items import search, similar_items
+import sys
+from pathlib import Path
+ROOT_DIR = Path(__file__).parent.parent.parent.__str__()
+sys.path.append(ROOT_DIR)
+from src.app.utils import cookies
+from src.app.api.items import search, similar_items
 
-class Vinted:
+class Vinted():
     def __init__(self):
         self.auth_cookie = cookies.load_auth_cookie() # [0] - Cookie name ||| [1] - Cookie value
         self.cookies = {self.auth_cookie[0]: self.auth_cookie[1]}
         
-    def search_items(self, **kwargs):
+    def search_items(self, **kwargs) -> dict:
         data = search(self.cookies, kwargs=kwargs)
         
         return data["items"]
     
-    def similar_items(self, item_id):
+    def similar_items(self, item_id: str) -> dict:
         data = similar_items(self.cookies, item_id=item_id)
         
         return data["items"]

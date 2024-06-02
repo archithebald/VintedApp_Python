d80e8749cb04136ba017cf8abeee480df41e324d
BuildTools <unconfigured@null.spigotmc.org>
2024-06-02 12:04:08 +0200

    Added more manuel tests ✅

--- src/app/Vinted.py
+++ src/app/Vinted.py
@@ -4,18 +4,25 @@ ROOT_DIR = Path(__file__).parent.parent.parent.__str__()
 sys.path.append(ROOT_DIR)
 from src.app.utils import cookies
 from src.app.api.items import search, similar_items
+from src.app.api.brand import get_brands, filter_brand 
 
 class Vinted():
     def __init__(self):
         self.auth_cookie = cookies.load_auth_cookie() # [0] - Cookie name ||| [1] - Cookie value
         self.cookies = {self.auth_cookie[0]: self.auth_cookie[1]}
         
-    def search_items(self, **kwargs) -> dict:
+    def search_items(self, **kwargs) -> list:
         data = search(self.cookies, kwargs=kwargs)
         
         return data["items"]
     
-    def similar_items(self, item_id: str) -> dict:
+    def similar_items(self, item_id: str) -> list:
         data = similar_items(self.cookies, item_id=item_id)
         
         return data["items"]
+    
+    def get_all_brands(self) -> list:
+        return get_brands()
+    
+    def get_brand(self, brand_id:int=None, brand_name:str=None, is_luxury:bool=None, requires_authenticity_check:bool=None) -> list:
+        return filter_brand(brand_id, brand_name, is_luxury, requires_authenticity_check)
---
+++ src/app/tests/test_get_all_brands.py
@@ -0,0 +1,9 @@
+import sys
+from pathlib import Path
+ROOT_DIR = Path(__file__).parent.parent.parent.parent.__str__()
+sys.path.append(ROOT_DIR)
+from src.app import Vinted
+
+app = Vinted.Vinted()
+
+print(app.get_all_brands())
